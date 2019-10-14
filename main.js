0f527b3357bafde9b76efa6b496b0c6894b0eeb0
august.moller <ag.man@live.se>
2019-10-14 15:32:32 +0200

    first commit

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ new Vue({
     el:'#root',
     data: data
 })
+
+
+function donebutton(){
+    document.getElementById("text").innerHTML = "Som en {{ roll }} vill jag {{ aktivitet }} i {{ sammanhang }} för att {{ orsak }}.";
+}
