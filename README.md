0a3d64cc554cf77c15a69e1c2bdde1d13bf4f4bc
august.moller <ag.man@live.se>
2019-10-15 15:32:52 +0200

    can add values to lists and full story

---
+++ README.md
@@ -0,0 +1,4 @@
+## user stories ##
+när alla inputs fylls i på story så läggs den in i en lista med genererade text baserad på inputs
+
+en lista på dem induviduella typerna t.ex roll, aktivitet sparas också separat i en lista och när man trycker på pilen io en input field så går dessav att välja.
--- main.js
+++ main.js
@@ -1,16 +1,75 @@
-let data = {
-    roll: '',
-    aktivitet: '',
-    sammanhang: '',
-    orsak: ''
-};
 
-new Vue({
+var app = new Vue({
     el:'#root',
-    data: data
+    data: {
+        newName: '',
+        names: [],
+        fullStories: [],
+        newRole: '',
+        roles: [],
+        newActivity: '',
+        activities: [],
+        newContext: '',
+        context: [],
+        newReason: '',
+        reasons: []
+    },
+
+    methods: {
+        addName(){
+            this.names.push("hello " + this.newName + this.newReason);
+
+            this.newName = '';
+        },
+
+        addStory(){
+            if(!this.newRole && !this.newActivity && !this.newContext && !this.newReason){
+                document.getElementById("error").innerHTML = "fill in everything to get full story";
+            } else if(this.newRole && this.newActivity && this.newContext && this.newReason){
+                document.getElementById("error").innerHTML = "full story added";
+                document.getElementById("full").innerHTML = "Full Story:";
+
+                this.fullStories.push(
+                "Som en " + this.newRole + 
+                " vill jag " + this.newActivity + 
+                " i " + this.newContext + 
+                " för att " + this.newReason + ".");
+
+            }
+
+            if (!this.newRole) {
+                console.log("Roll är tom")
+            }else if(this.newRole){
+                document.getElementById("rol").innerHTML = "roll:";
+                this.roles.push(this.newRole);
+                this.newRole = '';
+            }
+            
+            if(!this.newActivity){
+                console.log("Aktivitet är tom");
+            } else if(this.newActivity){
+                document.getElementById("akt").innerHTML = "aktivitet:";
+                this.activities.push(this.newActivity);
+                this.newActivity = '';
+            }
+
+            if(!this.newContext){
+                console.log("Sammanhang är tom");
+            } else if(this.newContext){
+                document.getElementById("con").innerHTML = "sammanhang:";
+                this.context.push(this.newContext);
+                this.newContext = '';
+            }
+            
+            if(!this.newReason){
+                console.log("Orsak är tom");
+            } else if(this.newReason){
+                document.getElementById("res").innerHTML = "orsak:";
+                this.reasons.push(this.newReason);
+                this.newReason = '';
+            }
+            
+            
+        }
+    },
 })
-
-
-function donebutton(){
-    document.getElementById("text").innerHTML = "Som en {{ roll }} vill jag {{ aktivitet }} i {{ sammanhang }} för att {{ orsak }}.";
-}
